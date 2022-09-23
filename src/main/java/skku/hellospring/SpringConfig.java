22b44719d2e7e7eff2ca547200a01f0de3554f07
jaehun <jaehun@jaehunui-MacBookAir.local>
2022-09-24 00:02:22 +0900

    jdbc

--- src/main/java/skku/hellospring/SpringConfig.java
+++ src/main/java/skku/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package skku.hellospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import skku.hellospring.repository.JdbcMemberRepository;
 import skku.hellospring.repository.MemberRepository;
 import skku.hellospring.repository.MemoryMemberRepository;
 import skku.hellospring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
